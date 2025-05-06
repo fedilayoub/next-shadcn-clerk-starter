0bfc689b0b21c248841c0a1e16023f0f7e2fab1b
ayoub.fedil <ayoub.fedil@tersea.com>
2025-05-06 15:00:21 +0100

    feat: add custom error page for handling errors

---
+++ app/error.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <section className="container mx-auto flex min-h-screen items-center justify-center p-24">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold">Something went wrong</h1>
+        <p className="text-lg mb-4">Please try again later.</p>
+        <Link
+          href="/"
+          className="flex items-center gap-x-1 text-blue-500 hover:text-blue-600 group"
+        >
+          <span className="pb-1"> Go back to the home page</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            className="w-5 h-5 fill-blue-500 group-hover:translate-x-1 transition-all duration-300"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      </div>
+    </section>
+  );
+}
