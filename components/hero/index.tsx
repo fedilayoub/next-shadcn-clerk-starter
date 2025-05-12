d4417f77918d625357537f5ddc75c31724f55a34
ayoub.fedil <ayoub.fedil@tersea.com>
2025-05-12 09:52:37 +0100

    feat: added hero content

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,7 +1,10 @@
 import { cn } from "@/lib/utils";
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
+
 const Hero = () => {
   return (
- <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+ <section className="relative flex h-[35rem] lg:h-[45rem] w-full items-center justify-center bg-white dark:bg-black">
       <div
         className={cn(
           "absolute inset-0",
@@ -11,10 +14,26 @@ const Hero = () => {
         )}
       />
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-       Hero Section
-      </p>
-    </div>
+      <div className="relative container mx-auto px-4  md:px-6 2xl:max-w-[1400px]">
+          <div className="mx-auto mt-5 max-w-2xl text-center">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl">
+              Focus On What Matters
+            </h1>
+          </div>
+          {/* End Title */}
+          <div className="mx-auto mt-5 max-w-3xl text-center">
+            <p className="text-muted-foreground text-xl">
+              This NextJS & Clerk custom auth component integration will save you hours of development time.
+            </p>
+          </div>
+          {/* Buttons */}
+          <div className="mt-8 flex justify-center gap-3">
+            <Link href="/sign-up" className="group flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-white hover:bg-primary/90 dark:bg-white dark:text-black">Start Building
+            <ArrowRightIcon className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
+            </Link>          
+          </div>
+        </div>
+    </section>
   )
 }
 
