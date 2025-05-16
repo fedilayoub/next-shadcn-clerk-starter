f413c40e492c96e626056daee9e2acd19e3f9fb7
ayoub.fedil <ayoub.fedil@tersea.com>
2025-05-16 16:53:22 +0100

    feat: added repo link

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,16 +1,16 @@
-'use client';
-import { useState } from 'react';
-import Link from 'next/link';
-import Logo from '@/components/logo';
-import { ThemeSwitcher } from '../theme-switcher';
-
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Logo from "@/components/logo";
+import { ThemeSwitcher } from "../theme-switcher";
+import { Button } from "@/components/ui/button";
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
   const navigation = [
-    { title: 'NavLink1', path: '/navlink1' },
-    { title: 'NavLink2', path: '/navlink2' },
-    { title: 'NavLink3', path: '/navlink3' },
-    { title: 'NavLink4', path: '/navlink4' },
+    { title: "NavLink1", path: "/navlink1" },
+    { title: "NavLink2", path: "/navlink2" },
+    { title: "NavLink3", path: "/navlink3" },
+    { title: "NavLink4", path: "/navlink4" },
   ];
   const toggleMenu = () => {
     setOpen(!isOpen);
@@ -21,8 +21,31 @@ const Header = () => {
         <div className="flex items-center justify-between py-5 px-4">
           <Logo withText />
           <div className="flex items-center gap-2">
-         <ThemeSwitcher /> 
-            <button className="menu-btn" onClick={toggleMenu}>
+            <Link
+              href="https://github.com/fedilayoub/next-shadcn-clerk-starter.git"
+              target="_blank"
+              title="GitHub"
+              rel="noopener noreferrer"
+              className="cursor-pointer flex items-center justify-center gap-x-1 py-3 px-4 text-white font-medium transform-gpu border border-zinc-300 bg-zinc-50 dark:bg-transparent dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset] backdrop-blur rounded-full md:inline-flex hover:bg-black/10 dark:hover:bg-white/10 transition-colors duration-300"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="text-black/70 dark:text-white"
+              >
+                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                <path d="M9 18c-4.51 2-5-2-7-2" />
+              </svg>
+            </Link>
+            <ThemeSwitcher />
+            <Button className="menu-btn" variant="outline" onClick={toggleMenu}>
               {isOpen ? (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -51,14 +74,12 @@ const Header = () => {
                   />
                 </svg>
               )}
-            </button>
+            </Button>
           </div>
         </div>
         {/* Mobile Menu  */}
         {isOpen && (
-          <div
-            className="mobile-menu fixed inset-0 z-50 bg-white dark:bg-zinc-900"
-          >
+          <div className="mobile-menu fixed inset-0 z-50 bg-white dark:bg-zinc-900">
             <div className="flex flex-col h-full">
               <div className="flex items-center justify-between p-4 border-b dark:border-zinc-800">
                 <Logo withText />
@@ -150,7 +171,30 @@ const Header = () => {
                   />
                 </svg>
               </Link>
-          <ThemeSwitcher />
+              <Link
+                href="https://github.com/fedilayoub/next-shadcn-clerk-starter.git"
+                target="_blank"
+                title="GitHub"
+                rel="noopener noreferrer"
+                className="cursor-pointer flex items-center justify-center gap-x-1 py-3 px-4 text-white font-medium transform-gpu border border-zinc-300 bg-zinc-50 dark:bg-transparent dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset] backdrop-blur rounded-full md:inline-flex hover:bg-black/10 dark:hover:bg-white/10 transition-colors duration-300"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-black/70 dark:text-white"
+                >
+                  <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                  <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
+              </Link>
+              <ThemeSwitcher />
             </div>
           </div>
         </div>
